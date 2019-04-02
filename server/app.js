5fa544bc08ca0886563dee584b739aafea3a644d
Evan Kaplan <evankaplan@fb.com>
2019-04-02 15:14:28 -0700

    Fix bug on handling path for bundle.js

--- server/app.js
+++ server/app.js
@@ -5,18 +5,17 @@ const model = require('../database/models/Room.js');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/rooms/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-app.get('/rooms/:id/*', (req, res) => {
+app.get('/bundle', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/bundle.js'));
 });
-
-app.use(morgan('dev'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/rooms/:id', (req, res) => {
   const { id } = req.params;
