89e76df9bd33ad37c91511efa63c3aab42a4b8af
Evan Kaplan <evankaplan@fb.com>
2019-04-02 13:17:47 -0700

    Refactor api routes to handle direct user queries to a room's endpoint

--- server/app.js
+++ server/app.js
@@ -5,17 +5,23 @@ const model = require('../database/models/Room.js');
 
 const app = express();
 
+app.get('/rooms/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+app.get('/rooms/:id/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/bundle.js'));
+});
+
 app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('/rooms/:id', (req, res) => {
+app.get('/api/rooms/:id', (req, res) => {
   const { id } = req.params;
   model.getRoomPhotos(id, (error, results) => {
-    if (error) {
+    if (results.length === 0) {
       res.status(500).end();
       return;
     }
