b71dec64e432ea6df2b6622cac22426d15f7fdbf
Evan Kaplan <evankaplan@fb.com>
2019-04-06 09:47:28 -0700

    Add toggle transitions to photo carousel

--- client/src/components/Caption.jsx
+++ client/src/components/Caption.jsx
@@ -6,27 +6,20 @@ const Caption = ({ photos, currentPhoto, handleClick, translateValue, showImageS
     <div className="caption-space">
       <div className="caption-top">
         <div className="photo-details">
-          <div className="photo-description">
+          <div>
             {`${currentPhoto.photoNum}/${photos.length}: ${currentPhoto.description}`}
           </div>
-          <div className="toggler-slider">
-            <button type="button" className="toggleImageSlider" name="toggleImageSlider" onClick={e => handleClick(e)}>
-              <div>
-                {showImageSlider ? "Hide photo list" : "Show photo list"} 
-              </div>
-              {showImageSlider
-                ? <img className="down" name="toggleImageSlider" src="./images/down-arrow.svg" alt="hide-nav" />
-                : <img className="down" name="toggleImageSlider" src="./images/up-arrow.svg" alt="show-nav" />
-               }
-            </button>
-          </div>
+          <button type="button" name="toggleImageSlider" onClick={e => handleClick(e)}>
+            {showImageSlider ? "Hide photo list" : "Show photo list"} 
+            {showImageSlider
+              ? <img className="toggle" name="toggleImageSlider" src="./images/down-arrow.svg" alt="hide-nav" />
+              : <img className="toggle" name="toggleImageSlider" src="./images/up-arrow.svg" alt="show-nav" />
+              }
+          </button>
         </div>
       </div>
-      <div className="thumbnail-view-container">
-        {showImageSlider 
-          ? <ImageSlider photos={photos} handleClick={handleClick} translateValue={translateValue} />
-          : <div />
-        }
+      <div className={showImageSlider ? "thumbnail-view-container open" : "thumbnail-view-container hidden"}>
+        <ImageSlider photos={photos} handleClick={handleClick} translateValue={translateValue} />
       </div>
     </div>
   </div>
