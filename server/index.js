ea8e473ba61f5b3ba08a11f672701a270406bef9
Evan Kaplan <evankaplan@fb.com>
2019-04-08 16:28:57 -0700

    Update port to serve photos module

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const db = require('../database/db.js'); // start db connection
 const app = require('./app.js');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
