cf782464fcdc0c84f6994f2ec970cb76c4692ba8
Evan Kaplan <evankaplan@fb.com>
2019-04-08 11:07:56 -0700

    Refactor to host svgs on s3

--- client/src/components/CarouselView.jsx
+++ client/src/components/CarouselView.jsx
@@ -6,14 +6,14 @@ const CarouselView = ({ photos, currentPhoto, handleClick, translateValue, showI
   <div className="carousel-page-container">
     <div className="return-container">
       <button type="button" className="return" name="return" onClick={e => handleClick(e)}>
-        <img className="return" name="return" src="./images/return.svg" alt="return-nav" />
+        <img className="return" name="return" src="https://s3-us-west-1.amazonaws.com/hrsf-fec/svg/return.svg" alt="return-nav" />
       </button>
     </div>
     <div className="main-content-container">
       <div className="main-photo-nav-container">
         <div className="nav-button-container">
           <button type="button" className="back" name="back" onClick={e => handleClick(e)}>
-            <img className="back" name="back" src="./images/left-arrow.svg" alt="back-nav" />
+            <img className="back" name="back" src="https://s3-us-west-1.amazonaws.com/hrsf-fec/svg/left-arrow.svg" alt="back-nav" />
           </button>
         </div>
         <div>
@@ -21,7 +21,7 @@ const CarouselView = ({ photos, currentPhoto, handleClick, translateValue, showI
         </div>
         <div className="nav-button-container">
           <button type="button" className="forward" name="forward" onClick={e => handleClick(e)}>
-            <img className="forward" name="forward" src="./images/right-arrow.svg" alt="forward-nav" />
+            <img className="forward" name="forward" src="https://s3-us-west-1.amazonaws.com/hrsf-fec/svg/right-arrow.svg" alt="forward-nav" />
           </button>
         </div>
       </div>
