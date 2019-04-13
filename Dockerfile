2169f7de5a6de29b8439fd68592a9fa902f60fec
Evan Kaplan <evankaplan@fb.com>
2019-04-13 11:14:30 -0700

    Improve route handling errors on ec2

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN npm install
 
 EXPOSE 3001
 
-CMD ["npm", "run", "start-docker"]
+CMD ["npm", "run", "start"]
--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
 const model = require('../database/models/Room.js');
 
 const app = express();
@@ -10,15 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/rooms/:id', express.static('client/dist'));
-app.use(express.static('client/dist'));
-
+app.use('/bundle.js', express.static('client/dist'));
 
 app.get('/api/photos/:id', (req, res) => {
   const { id } = req.params;
   model.getRoomPhotos(id, (error, results) => {
-    if (results.length === 0 || error) {
+    if (!results || (results.length === 0 || error)) {
       res.status(500).end();
-      return;
     }
     res.type('json').send(results);
   });
